623c61a3920c4cb6f15e61e69d98477127b21a56
Lord Ghapa <felipef32@hotmail.com>
2023-07-12 01:23:20 -0300

    creating component LogoLink

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,16 @@ module.exports = {
     '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
     '!<rootDir>/node_modules/',
+    '!<rootDir>/src/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/styles/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/Button/**',
   ],
   testEnvironment: 'jsdom',
-  testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test|tests).[tj]s?(x)',
-  ],
+  // testMatch: [
+  //   '**/__tests__/**/*.[jt]s?(x)',
+  //   '**/?(*.)+(spec|test|tests).[tj]s?(x)',
+  // ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
   // transform: {
