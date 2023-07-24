0e2df9587ee9f6b93fa91de5eb30568bbcdbe155
Lord Ghapa <felipef32@hotmail.com>
2023-07-24 00:15:58 -0300

    creating routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
 import Home from './templates/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PageNotFound } from './templates/PageNotFound';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
